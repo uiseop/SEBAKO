5047b43d51870059609cf4caec60caab32a9f3b0
uiseop <bono521@naver.com>
2021-05-10 16:53:27 +0900

    이력서 작성 페이지 및 함수들 사용

--- resumes/models.py
+++ resumes/models.py
@@ -8,6 +8,8 @@ class Resume(models.Model):
     issure = models.CharField(max_length=30, verbose_name='발급처')
     dateAcq = models.DateField()
 
+    file_upload = models.FileField(upload_to='resumes/files/%Y/%m/%d', blank=True)
+
     created_at = models.DateField(auto_now_add=True)
     updated_at = models.DateField(auto_now=True)
 
