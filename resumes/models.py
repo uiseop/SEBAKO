d5b3542a3c0b77bc6ba8bf09251ddfbebdf7a549
uiseop <bono521@naver.com>
2021-05-12 16:27:07 +0900

    자기소개 모델 필드 추가

--- resumes/models.py
+++ resumes/models.py
@@ -5,7 +5,38 @@ from django.db import models
 from django.urls import reverse
 
 
-class Resume(models.Model):
+class SelfIntro(models.Model): # 간단자기소개서 작성 및 개인 sns 주소 입력 필드
+    text = models.TextField(default="간단한 자기소개서를 작성해보세요.", verbose_name='간단소개')
+    sns = models.JSONField(default='{}',verbose_name='sns 종류 및 주소')
+    user = models.ForeignKey(User, null=False, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'[{self.user}] {self.text}'
+
+class Experience(models.Model): # 경험 작성 필드
+    title = models.CharField(max_length=30, verbose_name='직무')
+    company = models.CharField(max_length=30, verbose_name='회사/장소')
+    text = models.TextField(verbose_name='당시 한 일')
+    dateFrom = models.DateField(verbose_name='시작일')
+    dateEnd = models.DateField(verbose_name='종료일')
+    user = models.ForeignKey(User, null=False, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'[{self.user}] {self.title}'
+
+class Education(models.Model): # 학력 작성 필드
+    school = models.CharField(max_length=30, verbose_name='학교명')
+    major = models.CharField(max_length=30, verbose_name='전공명', null=True)
+    course = models.JSONField(default='{}', verbose_name='이수과목')
+    dateFrom = models.DateField(verbose_name='시작일')
+    dateEnd = models.DateField(verbose_name='종료일')
+    user = models.ForeignKey(User, null=False, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'[{self.user}] {self.school}'
+
+
+class Resume(models.Model): # 자격증 작성 필드
     title = models.CharField(max_length=30, verbose_name='자격증명')
     regiNum = models.CharField(max_length=30, verbose_name='자격증번호')
     issure = models.CharField(max_length=30, verbose_name='발급처')
@@ -16,7 +47,7 @@ class Resume(models.Model):
     created_at = models.DateField(auto_now_add=True)
     updated_at = models.DateField(auto_now=True)
 
-    user = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, null=False, on_delete=models.CASCADE)
 
     def __str__(self):
         return f'[{self.user}] {self.title}'
@@ -26,3 +57,4 @@ class Resume(models.Model):
 
     class Meta:
         ordering = ['-created_at']
+
